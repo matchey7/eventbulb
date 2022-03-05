27ba7e34287e3f7111743fe0db1b57d3db1a2551
Maciej Dybala <maciej.dybala@gmail.com>
2022-03-05 08:58:23 +0000

    Passing Down Data

--- events/views.py
+++ events/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from datetime import datetime
+from .models import Event
 # Create your views here.
 
 def details(request):
     return render(request, 'events/details.html')
 
 def events_list(request):
-    return render(request, 'events/events_list.html')
+    events = Event.objects.all()
+    return render(request, 'events/events_list.html', {"events": events})
